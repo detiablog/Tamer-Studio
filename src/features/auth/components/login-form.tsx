ed7195d2c3ab68846c421b62da254942480ebbb6
Tamer Desa <aoneshoper@gmail.com>
2026-07-20 14:27:17 +0700

    update login process

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -1,11 +1,24 @@
 "use client";
 
+import { useState } from "react";
+import { authClient } from "@/lib/auth/auth-client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 
 export function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async () => {
+  const result = await authClient.signIn.email({
+    email,
+    password,
+  });
+
+  console.log(result);
+};
   return (
     <Card className="w-full max-w-md">
       <CardHeader>
@@ -15,17 +28,32 @@ export function LoginForm() {
       <CardContent className="space-y-4">
         <div className="space-y-2">
           <Label>Email</Label>
-          <Input type="email" />
+
+          <Input
+            type="email"
+            placeholder="Masukkan email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
 
         <div className="space-y-2">
           <Label>Password</Label>
-          <Input type="password" />
+
+          <Input
+            type="password"
+            placeholder="Masukkan password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
-        <Button className="w-full">
-          Sign In
-        </Button>
+  <Button
+  className="w-full"
+  onClick={handleLogin}
+>
+  Sign In
+  </Button>
       </CardContent>
     </Card>
   );
